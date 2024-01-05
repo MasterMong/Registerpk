befdf75421956789b9be7ca8e20fbb57a8a5494a
base_happy19 <43717543+basehappy19@users.noreply.github.com>
2024-01-05 19:54:37 +0700

    Add files via upload

--- account.php
+++ account.php
@@ -42,7 +42,7 @@ require('helper/server/checkplan.php')
                         <?php
                     }
                     ?>
-                    <div><span>คะแนนความประพฤติ : </span><?php echo $_SESSION['point'];?>คะแนน</div>
+                    <div><span>คะแนนความประพฤติ : </span><?php echo $_SESSION['point'];?> คะแนน</div>
                     <hr>
                 <?php
                 } else {
