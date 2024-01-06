c0854246a67c8fcddb1faddbea36ac4c57a20a6a
base_happy19 <43717543+basehappy19@users.noreply.github.com>
2024-01-07 04:43:39 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="helper/style.css">
     <link rel="stylesheet" href="helper/home.css">
+    <script src="https://kit.fontawesome.com/5134196601.js" crossorigin="anonymous"></script>
 </head>
 <body>
 
